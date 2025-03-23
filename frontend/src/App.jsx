44cc24bfc66d6b74762e864e2bbae562547e0605
khushboo8252 <khushbooranjan8252@gmail.com>
2025-03-23 22:28:11 +0530

    add frontend part

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen p-6">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
