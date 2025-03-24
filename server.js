6a6a9f8f290f12efbce951812b860971c5ff592a
Khushboo Kumari <143016224+khushboo8252@users.noreply.github.com>
2025-03-24 16:01:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,12 +10,9 @@ app.get("/scrape", async (req, res) => {
   if (!url) return res.status(400).json({ error: "URL is required" });
 
   try {
-    console.log(`Scraping started for: ${url}`);
     const data = await scrapeAmazonProduct(url);
-    console.log("Scraped Data:", data);
     res.json(data);
   } catch (error) {
-    console.error("Scraping Error:", error);
     res.status(500).json({ error: "Scraping failed", details: error.message });
   }
 });
